cb74cd103887b9eb5eb738a6b8827660a6006643
Waldir R Pires Jr <wrpires@fitec.org.br>
2016-12-29 17:26:38 -0200

    fixed bugs

---
+++ tests/redeem-code/.settings/sonarlint/redeem-code/index.pb
@@ -0,0 +1,17 @@
+
+Z
+*src/test/java/org/wpjr2/xbox/XBoxTest.java,1\b\1b799527af8a6a47f1f8248f4cc0a13cfa7a6289
+9
+	log4j.xml,3\7\379f8e5b1e05c4fae4b2e1d5e13bbf293b155c88
+g
+7src/main/java/org/wpjr2/xbox/redeemcode/RedeemCode.java,c\8\c83fac774fc5074e44032f6b934f30fb4f133ad9
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+^
+.src/test/java/org/wpjr2/xbox/SeleniumTest.java,5\e\5e2d4ae362fd3e74ea722725fb0f29a20be3d1bb
+`
+0src/test/java/org/wpjr2/xbox/SeleniumThread.java,4\2\4211e01b144705fbf43827fcb9cefa76a7c62862
+_
+/src/test/java/org/wpjr2/xbox/SeleniumTest2.java,9\a\9a59d5a5289b197a839b81f0310e63ce72fc7a53
+g
+7src/main/java/org/wpjr2/xbox/redeemcode/Downloader.java,0\d\0dff948f7aa1cc2b1301649442fc9225684ff54f
--- tests/redeem-code/src/test/java/org/wpjr2/xbox/SeleniumTest.java
+++ tests/redeem-code/src/test/java/org/wpjr2/xbox/SeleniumTest.java
@@ -1,8 +1,10 @@
 package org.wpjr2.xbox;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.junit.After;
 import org.junit.Before;
@@ -35,9 +37,29 @@ public class SeleniumTest {
 		selenium.waitForPageToLoad("30000");
 		
 		StringBuffer code = new StringBuffer("437KY-*&X3WKY4GJ");
-		String chars1 = "KLMNOPQRSTUVWXYZ0123456789";
+//		String chars1 = "KLMNOPQRSTUVWXYZ0123456789";
+//		String chars2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+//		String chars3 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		String chars0 = "P";
+		String chars1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 		String chars2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 		String chars3 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		Set<String> codes = new TreeSet<String>();
+		for (int h = 0; h < chars0.length(); h++){
+			code.replace(4,5, chars0.charAt(h)+"");
+		for (int i = 0; i < chars1.length(); i++){
+			code.replace(5,6, chars1.charAt(i)+"");
+			for (int k = 0; k < chars2.length(); k++){
+				code.replace(6,7, chars2.charAt(k)+"");
+				for (int l = 0; l < chars3.length(); l++){
+					code.replace(7,8, chars3.charAt(l)+"");
+					codes.add(code.toString());
+				}				
+			}
+		}
+		}
+		System.out.println("Codes: " + codes.size());
+		int count = 0;
 		selenium.click("id=primary_opt_in");
 		selenium.click("id=rules_checkbox");
 		selenium.type("id=email_2", "wrpiresjr@outlook.com");
@@ -46,30 +68,27 @@ public class SeleniumTest {
 		selenium.select("name=age.birth_month", "label=Sep");
 		selenium.select("name=age.birth_year", "label=1976");
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd hh:mm:ss");
-		for (int i = 0; i < chars1.length(); i++){
-			code.replace(5,6, chars1.charAt(i)+"");
-			for (int k = 0; k < chars2.length(); k++){
-				code.replace(6,7, chars2.charAt(k)+"");
-				for (int l = 0; l < chars3.length(); l++){
-					code.replace(7,8, chars3.charAt(l)+""); 
-					selenium.type("id=purchase_code", code.toString());
-					selenium.click("css=div.profile > div.row > div.column.small-12 > div.submit > button[type=\"submit\"]");
-					selenium.waitForPageToLoad("30000");
-					WebElement element = driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/div[2]/div[2]/form/div/div[1]/div/p[2]"));
-					if (element != null){
-						if (element.getText().contains("Desculpe, este c") ||
-								element.getText().contains("Sorry, this is not a valid code"))
-						{
-							System.out.println(sdf.format(
-									Calendar.getInstance().getTime())+ " : " +  code + ": invalid: ");
-						} else {
-							System.out.println(" valid: ");
-							System.exit(0);
-						}
-					}
-				}				
+		for (String c: codes){
+			selenium.type("id=purchase_code", c.toString());
+			selenium.click("css=div.profile > div.row > div.column.small-12 > div.submit > button[type=\"submit\"]");
+			selenium.waitForPageToLoad("30000");
+			WebElement element = driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/div[2]/div[2]/form/div/div[1]/div/p[2]"));
+			if (element != null){
+				if (element.getText().contains("Desculpe, este c") ||
+					element.getText().contains("Sorry, this is not a valid code"))
+				{
+					System.out.println(String.format("%s: %d/%d - %s -> %s", sdf.format(
+							Calendar.getInstance().getTime()), count, codes.size(), c, "INVALID"));
+				} else {
+					System.out.println(String.format("%s: %d/%d - %s -> %s", sdf.format(
+							Calendar.getInstance().getTime()), count, codes.size(), c, "VALID"));
+					File f = new File(c);
+					f.createNewFile();
+					System.exit(0);
+				}
 			}
-		}
+			count++;
+		}				
 	}
 
 	@After
